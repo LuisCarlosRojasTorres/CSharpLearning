0482cc1cd53da28e071e07b12b009d8738693b35
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2023-01-09 19:07:23 -0300

    Dummy_LiteDB Project Finished
    List of changes:
    - added INSERT
    - added GET
    - added UPDATE
    - added DELETE
    
    List of TODOs:
    - Add how to use LINQ maybe in another project...

---
+++ Dummy_LiteDB/Dummy_LiteDB/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dummy_LiteDB
+{
+    internal class Pessoa
+    {
+
+        public Pessoa(string nome, int idade) 
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+        } 
+
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public int Idade { get; set; }
+    }
+}
--- Dummy_LiteDB/Dummy_LiteDB/Program.cs
+++ Dummy_LiteDB/Dummy_LiteDB/Program.cs
@@ -1,2 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using LiteDB;
+using Dummy_LiteDB;
+using System.Globalization;
+
+static LiteDatabase CreateDB()
+{
+    LiteDatabase db = new LiteDatabase("Lite.db");
+    return db;
+}
+
+static void Add(ref LiteDatabase db, string nome, int idade)
+{
+    db.GetCollection<Pessoa>().Insert(new Pessoa(nome, idade));
+    db.Commit();
+}
+
+static void Get(ref LiteDatabase db, string nome)
+{
+    if (db.CollectionExists("Pessoa")) 
+    {
+        var foundOne = db.GetCollection<Pessoa>().FindOne(x => x.Nome == nome);
+        Console.WriteLine($" > GET, Id: {foundOne.Id} , Nome: {foundOne.Nome} , Idade: {foundOne.Idade}");
+    }
+}
+
+static void UpdateIdade (ref LiteDatabase db, string nome, int idade)
+{
+    if (db.CollectionExists("Pessoa"))
+    {
+        var foundOne = db.GetCollection<Pessoa>().FindOne(x => x.Nome == nome);
+        foundOne.Idade = idade;
+        db.GetCollection<Pessoa>().Update(foundOne);
+        Console.WriteLine($" > UPDATE, Id: {foundOne.Id} , Nome: {foundOne.Nome} , Idade: {foundOne.Idade}");
+    }
+}
+
+static void Delete(ref LiteDatabase db, string nome)
+{
+    if (db.CollectionExists("Pessoa"))
+    {
+        var foundOne = db.GetCollection<Pessoa>().FindOne(x => x.Nome == nome);
+        Console.WriteLine($" > DELETED, Id: {foundOne.Id} , Nome: {foundOne.Nome} , Idade: {foundOne.Idade}");
+        db.GetCollection<Pessoa>().Delete(foundOne.Id);
+        // Deleted
+    }
+}
+
+LiteDatabase db = CreateDB();
+//Add(ref db, "Luis Rojas", 34);
+///Add(ref db, "Jessy Morier", 32);
+Add(ref db, "Rufo", 10);
+Get(ref db, "Rufo");
+UpdateIdade(ref db, "Rufo", 32);
+Get(ref db, "Rufo");
+Delete(ref db, "Rufo");
+
+
+
+
+
+
+
+
+
