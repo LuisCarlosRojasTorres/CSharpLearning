c9b738c6e69f0f6476804f1882f734e6725d46c4
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2023-01-09 20:49:16 -0300

    Dummy_LINQ Project created List of changes: - Added project for LINQ test - Added dummy data
    List of TODOs:
    - Add SELECT commands

---
+++ Dummy_LINQ/Dummy_LINQ/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+var customer = new[]
+{
+    new{ CustomerID = 1, FirstName = "Lala", LastName = "Papata", Company = "Facabaaka"},
+    new{ CustomerID = 2, FirstName = "Lele", LastName = "Pepete", Company = "Fecebeeke"},
+    new{ CustomerID = 3, FirstName = "Lili", LastName = "Pipiti", Company = "Ficibiiki"},
+    new{ CustomerID = 4, FirstName = "Lolo", LastName = "Popoto", Company = "Focobooko"},
+    new{ CustomerID = 5, FirstName = "Lulu", LastName = "Puputu", Company = "Fucubuuku"},
+    new{ CustomerID = 6, FirstName = "Lalo", LastName = "Papota", Company = "Facabaaka"},
+    new{ CustomerID = 7, FirstName = "Lelo", LastName = "Pepote", Company = "Fecebeeke"},
+    new{ CustomerID = 8, FirstName = "Lila", LastName = "Pipati", Company = "Ficibiiki"}
+};
+
+var addresses = new[]
+{
+    new{ Company = "Facabaaka", City = "Lama"},
+    new{ Company = "Fecebeeke", City = "Leme"},
+    new{ Company = "Ficibiiki", City = "Limi"},
+    new{ Company = "Focobooko", City = "Lomo"},
+    new{ Company = "Fucubuuku", City = "Lumu"}
+};
