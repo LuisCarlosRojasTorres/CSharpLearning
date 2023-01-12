c421d28c912f082ad0699675beda06d249184e57
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2023-01-12 05:35:37 -0300

    Dummy_LiteDB Methods improved
    List of changes:
    - Updated GET, now it verifies if null is returned . otherwise
        a message will be show
    - Updated UPDATE, now it verifies if null is returned, otherwise
        ADD is called
    - Updated DELETE, now it verifies if null is returned, otherwise
        a message will be show
    
    List of TODOs:
    - Add a project of how to interact with json files

--- Dummy_LiteDB/Dummy_LiteDB/Program.cs
+++ Dummy_LiteDB/Dummy_LiteDB/Program.cs
@@ -13,6 +13,7 @@ static void Add(ref LiteDatabase db, string nome, int idade)
 {
     db.GetCollection<Pessoa>().Insert(new Pessoa(nome, idade));
     db.Commit();
+    Get(ref db, nome);
 }
 
 static void Get(ref LiteDatabase db, string nome)
@@ -20,7 +21,15 @@ static void Get(ref LiteDatabase db, string nome)
     if (db.CollectionExists("Pessoa")) 
     {
         var foundOne = db.GetCollection<Pessoa>().FindOne(x => x.Nome == nome);
-        Console.WriteLine($" > GET, Id: {foundOne.Id} , Nome: {foundOne.Nome} , Idade: {foundOne.Idade} , DateTime: {foundOne.Fecha}");
+        if (foundOne != null)
+        {
+            Console.WriteLine($" > GET, Id: {foundOne.Id} , Nome: {foundOne.Nome} , Idade: {foundOne.Idade} , DateTime: {foundOne.Fecha}");
+        }
+        else 
+        {
+            Console.WriteLine($" > GET ERROR: {nome} not Found, ");
+        }
+        
     }
 }
 
@@ -29,9 +38,18 @@ static void UpdateIdade (ref LiteDatabase db, string nome, int idade)
     if (db.CollectionExists("Pessoa"))
     {
         var foundOne = db.GetCollection<Pessoa>().FindOne(x => x.Nome == nome);
-        foundOne.Idade = idade;
-        db.GetCollection<Pessoa>().Update(foundOne);
-        Console.WriteLine($" > UPDATE, Id: {foundOne.Id} , Nome: {foundOne.Nome} , Idade: {foundOne.Idade} , DateTime: {foundOne.Fecha}");
+        if (foundOne != null)
+        {
+            foundOne.Idade = idade;
+            db.GetCollection<Pessoa>().Update(foundOne);
+            Console.WriteLine($" > UPDATE, Id: {foundOne.Id} , Nome: {foundOne.Nome} , Idade: {foundOne.Idade} , DateTime: {foundOne.Fecha}");
+        }
+        else 
+        {
+            Console.WriteLine($" > UPDATE ERROR: {nome} not Found, using Add instead");
+            Add(ref db, nome, idade);
+        }
+        
     }
 }
 
@@ -40,24 +58,31 @@ static void Delete(ref LiteDatabase db, string nome)
     if (db.CollectionExists("Pessoa"))
     {
         var foundOne = db.GetCollection<Pessoa>().FindOne(x => x.Nome == nome);
-        Console.WriteLine($" > DELETED, Id: {foundOne.Id} , Nome: {foundOne.Nome} , Idade: {foundOne.Idade} , DateTime: {foundOne.Fecha}");
-        db.GetCollection<Pessoa>().Delete(foundOne.Id);
-        // Deleted
+        if (foundOne != null)
+        {
+            Console.WriteLine($" > DELETED, Id: {foundOne.Id} , Nome: {foundOne.Nome} , Idade: {foundOne.Idade} , DateTime: {foundOne.Fecha}");
+            db.GetCollection<Pessoa>().Delete(foundOne.Id);
+        }
+        else
+        {
+            Console.WriteLine($" > DELETE ERROR: {nome} not Found");            
+        }
     }
 }
 
 LiteDatabase db = CreateDB();
-Add(ref db, "Luis Rojas", 34);
-Add(ref db, "Jessy Morier", 32);
-Add(ref db, "Rufo", 10);
+//Add(ref db, "Luis Rojas", 34);
+//Add(ref db, "Jessy Morier", 32);
+//Add(ref db, "Rufo", 10);
 
 Get(ref db, "Luis Rojas");
 Get(ref db, "Jessy Morier");
 Get(ref db, "Rufo");
 
-//UpdateIdade(ref db, "Rufo", 32);
-//Get(ref db, "Rufo");
-//Delete(ref db, "Rufo");
+UpdateIdade(ref db, "Rufo", 10);
+UpdateIdade(ref db, "Le Raf", 8);
+
+
 //Delete(ref db, "Luis Rojas");
 //Delete(ref db, "Jessy Morier");
 
