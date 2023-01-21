3504cee1552a70b966e26591cd8f2e043387648f
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2023-01-21 09:37:46 -0300

    Dummy ParallelProgrammingAndConcurrency CH1
    Changelog:
    -
    
    List of changes:
    - Added Solution to repository
    - Added Example from CH01A
    
    List of TODOs
    - Add all examples from CH01

---
+++ ParallelProgAndConcurrency/CH01A/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var bgThread = new Thread(() =>
+{
+    while (true)
+    {
+        bool isNetworkUp = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+        Console.WriteLine($"Is network available? Answer:{isNetworkUp}");
+        Thread.Sleep(200);
+    }
+});
+
+bgThread.IsBackground = true; // Background property
+bgThread.Start();
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Main thread working...");
+    Task.Delay(10);
+}
+
+Console.WriteLine("Done");
+Console.ReadKey();
