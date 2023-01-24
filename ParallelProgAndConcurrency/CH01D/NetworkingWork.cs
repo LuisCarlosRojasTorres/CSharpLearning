02370a05cea9d295c8d0b6282a7daba7701241ca
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2023-01-24 05:28:54 -0300

    Dummy ParallelProgrammingAndConcurrency CH1
    Changelog:
    - Added Solution to repository
    - Added Example from CH01A
    - Added Example from CH01B
    - Added Example from CH01C
    
    List of changes:
    - Added Example from CH01D
    
    List of TODOs
    - Add all examples from CH01

---
+++ ParallelProgAndConcurrency/CH01D/NetworkingWork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH01D
+{
+    internal class NetworkingWork
+    {
+        public void CheckNetworkStatus(object data)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                bool isNetworkUp = System.Net.
+                NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+                Console.WriteLine($"Thread priority {(string)data}; Is network available? Answer: {isNetworkUp}");
+                i++;
+            }
+        }
+    }
+}
---
+++ ParallelProgAndConcurrency/CH01D/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CH01D;
+
+var networkingWork = new NetworkingWork();
+
+var bgThread1 = new Thread(networkingWork.CheckNetworkStatus);
+var bgThread2 = new Thread(networkingWork.CheckNetworkStatus);
+var bgThread3 = new Thread(networkingWork.CheckNetworkStatus);
+var bgThread4 = new Thread(networkingWork.CheckNetworkStatus);
+var bgThread5 = new Thread(networkingWork.CheckNetworkStatus);
+
+bgThread1.Priority = ThreadPriority.Lowest;
+bgThread2.Priority = ThreadPriority.BelowNormal;
+bgThread3.Priority = ThreadPriority.Normal;
+bgThread4.Priority = ThreadPriority.AboveNormal;
+bgThread5.Priority = ThreadPriority.Highest;
+
+bgThread1.Start("Lowest");
+bgThread2.Start("BelowNormal");
+bgThread3.Start("Normal");
+bgThread4.Start("AboveNormal");
+bgThread5.Start("Highest");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Main thread working...");
+}
+
+Console.WriteLine("Done");
+Console.ReadKey();
