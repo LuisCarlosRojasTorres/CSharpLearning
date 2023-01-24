9be7c50589e99c096dc6694231be43090ce79e09
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2023-01-24 06:32:47 -0300

    Dummy ParallelProgrammingAndConcurrency CH1
    Changelog:
    - Added Solution to repository
    - Added Example from CH01A
    - Added Example from CH01B
    - Added Example from CH01C
    - Added Example from CH01D
    
    List of changes:
    - Added Example from CH01E
    
    List of TODOs
    - Add all examples from CH01

---
+++ ParallelProgAndConcurrency/CH01E/NetworkingWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH01E
+{
+    internal class NetworkingWork
+    {
+        public void CheckNetworkStatus(object data)
+        {
+            var cancelToken = (CancellationToken)data;
+            while (!cancelToken.IsCancellationRequested)
+            {
+                bool isNetworkUp = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+                Console.WriteLine($"Is network available? Answer: { isNetworkUp}");
+            }
+        }
+    }
+}
---
+++ ParallelProgAndConcurrency/CH01E/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CH01E;
+
+var networkingWork = new NetworkingWork();
+var pingThread = new Thread(networkingWork.CheckNetworkStatus);
+var ctSource = new CancellationTokenSource();
+pingThread.Start(ctSource.Token);
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Loop: {i} Main thread working...");
+    Thread.Sleep(100);
+}
+Console.WriteLine($"Loop finished");
+ctSource.Cancel();
+pingThread.Join();
+ctSource.Dispose();
