c1dc8e52f74135eb2a91d6640e97bd666aa492cf
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2023-02-11 19:46:18 -0300

    Dummy_DBVariable
    Changelog:
    - Added reference to LiteDB
    
    List of Changes:
    - Add DBVariableClass
    
    List of TODOs:
    
    - Add Methods for database handling

---
+++ Dummy_DBVariable/Dummy_DBVariable/DBVariable.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dummy_DBVariable
+{
+    public class DBVariable
+    {
+        public string Key { get; set; }
+
+        public DateTime[] Dates { get; set; }
+        public string[] Values { get; set; }
+        public bool[] Hidden { get; set; }
+
+        public DBVariable(string key, string value, bool hidden = false) 
+        {
+            this.Key = key;
+            DateTime now= DateTime.Now;
+            
+            this.Dates= new DateTime[1];
+            this.Values= new string[1];
+            this.Hidden = new bool[1];
+
+            this.Dates[0] = now;
+            this.Values[0] = value;
+            this.Hidden[0] = hidden;
+        }
+
+        public void AddValue( string value, bool hidden = false)
+        {
+            int size = this.Dates.Length;
+
+            DateTime now = DateTime.Now;
+
+            DateTime[] dummyDates = new DateTime[size+1];
+            String[] dummyValues = new string[size+1];
+            bool[] dummyHidden = new bool[size+1];
+
+            dummyDates.CopyTo(this.Dates, 0);
+            dummyDates[size - 1] = now;
+
+            dummyValues.CopyTo(this.Values, 0);
+            dummyValues[size - 1] = value;
+
+            dummyHidden.CopyTo(this.Hidden, 0);
+            dummyHidden[size - 1] = hidden;
+
+            this.Dates= dummyDates;
+            this.Values= dummyValues;
+            this.Hidden= dummyHidden;
+        }
+
+        public void SetAsHidden() 
+        {
+            for(int i = 0; i < this.Hidden.Length; i++) 
+            {
+                this.Hidden[i] = true;
+            }
+        }
+
+        public override string ToString()
+        {
+            string ans = "KEY: " + Key + "\n";
+
+            for (int i = 0; i < Dates.Length; i++) {
+                ans += " - " + Dates[i].ToString() + " , " + Values[i] + " , " + Hidden[i].ToString(); 
+            }
+
+            return ans;
+        }
+    }
+}
--- Dummy_DBVariable/Dummy_DBVariable/Program.cs
+++ Dummy_DBVariable/Dummy_DBVariable/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using Dummy_DBVariable;
+using LiteDB;
+
+static void AddData(string key, string value)
+{
+    using (var liteDb = new LiteDatabase(Path.GetFullPath("Rufo.db"))) 
+    {
+        if (liteDb.CollectionExists(key))
+        {
+            //Update
+            var foundOne = liteDb.GetCollection<DBVariable>(key).FindOne(x => x.Key == key);
+            foundOne.AddValue(value);
+
+            
+        }
+        else 
+        {
+            var foundOne = liteDb.GetCollection<DBVariable>(key);
+            var variable = new DBVariable(key, value);
+            foundOne.Insert(variable);
+        }
+    }
+    
+}
+
+
+
