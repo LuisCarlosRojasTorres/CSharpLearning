cd245b0eea0e5a90e5c42c5e04554aad43678ef0
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2023-02-12 18:26:47 -0300

    Dummy_DBVariableList
    Changelog:
    - Added Dummy_DBVariable proj: It uses arrays for data.
    - Add DBVariable class
    - Add Methods for database handling
    - Add DBVariable2 Class: Array
        - Added RufoArray.db
    
    List of Changes:
    - Add DBVariable Class: It uses List for data!
        - Added RufoList.db
    
    List of TODOs:
    -

---
+++ Dummy_DBVariable/Dummy_DBVariableList/DBVariable.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dummy_DBVariableList
+{
+    public class DBVariable
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+
+        public List<string>? Dates { get; set; }
+        public List<string>? Values { get; set; }
+        public List<bool>? Hidden { get; set; }
+
+        public DBVariable()
+        {
+            this.Key = "dummyKey";
+
+            this.Dates = new List<string> { DateTime.Now.ToString() };
+            this.Values = new List<string> { "dummyValue" };
+            this.Hidden = new List<bool> { false };
+        }
+
+        public DBVariable(string key, List<string> dates, List<string> values, List<bool> hidden)
+        {
+            this.Key = key;
+
+            this.Dates = dates;
+            this.Values = values;
+            this.Hidden = hidden;
+        }
+
+        public DBVariable(string key, string value, bool hidden = false)
+        {
+            this.Key = key;
+
+            this.Dates = new List<string> { DateTime.Now.ToString() };
+            this.Values = new List<string> { value };
+            this.Hidden = new List<bool> { hidden };
+        }
+
+        public void AddValue(string value, bool hidden = false)
+        {
+            this.Dates.Add(DateTime.Now.ToString());
+            this.Values.Add(value);
+            this.Hidden.Add(hidden);
+        }
+
+        public void SetAsHidden()
+        {
+            for (int i = 0; i < this.Hidden.Count; i++)
+            {
+                this.Hidden[i] = true;
+            }
+        }
+
+        public string GetLastValue()
+        {
+            if (this.Values != null)
+            {
+                return this.Values[this.Values.Count() - 1];
+            }
+            else 
+            {
+                return "null reference!";
+            }
+            
+        }
+
+        public override string ToString()
+        {
+            string ans = "KEY: " + Key + "\n";
+
+            for (int i = 0; i < Dates.Count; i++)
+            {
+                ans += " - " + Dates[i] + " , " + Values[i] + " , " + Hidden[i].ToString() + "\n";
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ Dummy_DBVariable/Dummy_DBVariableList/Program.cs
@@ -0,0 +1,108 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Dummy_DBVariableList;
+using LiteDB;
+
+static void AddData(string key, string value)
+{
+    //using (var liteDb = new LiteDatabase(Path.GetFullPath("Rufo.db"))) 
+    using (var liteDb = new LiteDatabase("RufoList.db"))
+    {
+        if (liteDb.CollectionExists("DBVariable"))
+        {
+            //Update
+            var foundOne = liteDb.GetCollection<DBVariable>().FindOne(x => x.Key == key);
+
+            if (foundOne != null)
+            {
+                foundOne.AddValue(value);
+                liteDb.GetCollection<DBVariable>().Update(foundOne);
+            }
+            else
+            {
+                liteDb.GetCollection<DBVariable>().Insert(new DBVariable(key, value));
+            }
+        }
+        else
+        {
+            //Add
+            liteDb.GetCollection<DBVariable>().Insert(new DBVariable(key, value));
+        }
+    }
+}
+
+static void printVariable(string key)
+{
+    using (var liteDb = new LiteDatabase(Path.GetFullPath("RufoList.db")))
+    {
+        if (liteDb.CollectionExists("DBVariable"))
+        {
+            var foundOne = liteDb.GetCollection<DBVariable>().FindOne(x => x.Key == key);
+            Console.WriteLine(foundOne);
+        }
+        else
+        {
+            Console.WriteLine("Se llevaron todo, todillo");
+        }
+    }
+}
+
+static void setAsHidden(string key)
+{
+    using (var liteDb = new LiteDatabase(Path.GetFullPath("RufoList.db")))
+    {
+        if (liteDb.CollectionExists("DBVariable"))
+        {
+            var foundOne = liteDb.GetCollection<DBVariable>().FindOne(x => x.Key == key);
+            foundOne.SetAsHidden();
+            liteDb.GetCollection<DBVariable>().Update(foundOne);
+            Console.WriteLine(foundOne);
+
+        }
+        else
+        {
+            Console.WriteLine("Se llevaron todo, todillo");
+        }
+    }
+}
+
+static string getLastValue(string key)
+{
+    using (var liteDb = new LiteDatabase(Path.GetFullPath("RufoList.db")))
+    {
+        if (liteDb.CollectionExists("DBVariable"))
+        {
+            var foundOne = liteDb.GetCollection<DBVariable>().FindOne(x => x.Key == key);
+            string lastValue = foundOne.GetLastValue();            
+            return lastValue;
+        }
+        else
+        {
+            return "";   
+        }
+    }
+}
+/*
+AddData("Rufo", "311");
+AddData("Rufo", "321");
+AddData("Rufo", "331");
+AddData("Rufo", "341");
+AddData("Rufo", "351");
+AddData("Rufo", "361");
+
+
+AddData("Lori", "211");
+AddData("Lori", "221");
+AddData("Lori", "231");
+AddData("Lori", "241");
+
+
+AddData("Lori2", "211");
+AddData("Lori2", "222");
+AddData("Lori2", "233");
+AddData("Lori2", "244");
+AddData("Lori2", "255");
+AddData("Lori2", "26");
+*/
+
+//printVariable("Lori2");
+Console.WriteLine(getLastValue("Rufo"));
