a6e0df39db97f6964a07046a2f4e05f9eb62301a
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2023-03-09 08:44:38 -0300

    LiteDBStorage How much storage is 3 years
    Changelog:
    
    List of Changes:
    - Added Project
    
    List of TODOs:
    - Add DBVariableStorage class
        - Add random timestamp
    - Add Test with 6 000 data (a day)
    - Add Test with 180 000 data (a month)
    - Add Test with 2 200 000 data (a year)
    - Add Test with 6 600 000 data (3 years)

---
+++ ProgrammingASPNETCore/CH02/CH02/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using CH02.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CH02.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new ContentResult { Content = "This is the Home.Index" };
+        }        
+    }
+}
