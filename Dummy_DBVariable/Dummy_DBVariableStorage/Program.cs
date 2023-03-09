d5fa8f9f5984ceac841a11e9a3df0b3119256a8a
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2023-03-09 11:48:02 -0300

    LiteDBStorage How much storage is 3 years
    Changelog:
    - Added Project
    - Added DBVariableStorage class
        - Added random timestamp
    
    List of Changes:
    - Added Test with 6 000 data (a day)
    - Added Test with 180 000 data (a month)
    - Added Test with 2 200 000 data (a year)
    - Added Test with 6 600 000 data (3 years)
    
    List of TODOs:

--- Dummy_DBVariable/Dummy_DBVariableStorage/Program.cs
+++ Dummy_DBVariable/Dummy_DBVariableStorage/Program.cs
@@ -1,13 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LiteDB;
-using Dummy_DBVariableStorage
+using Dummy_DBVariableStorage;
 
 static void AddData(string key, string value)
 {
     //using (var liteDb = new LiteDatabase(Path.GetFullPath("Rufo.db"))) 
-    using (var liteDb = new LiteDatabase("RufoList.db"))
+    using (var liteDb = new LiteDatabase("RufoStorage.db"))
     {
-        if (liteDb.CollectionExists("DBVariable"))
+        if (liteDb.CollectionExists("DBVariableStorage"))
         {
             //Update
             var foundOne = liteDb.GetCollection<DBVariableStorage>().FindOne(x => x.Key == key);
@@ -32,9 +32,9 @@ static void AddData(string key, string value)
 
 static void printVariable(string key)
 {
-    using (var liteDb = new LiteDatabase(Path.GetFullPath("RufoList.db")))
+    using (var liteDb = new LiteDatabase(Path.GetFullPath("RufoStorage.db")))
     {
-        if (liteDb.CollectionExists("DBVariable"))
+        if (liteDb.CollectionExists("DBVariableStorage"))
         {
             var foundOne = liteDb.GetCollection<DBVariableStorage>().FindOne(x => x.Key == key);
             Console.WriteLine(foundOne);
@@ -50,7 +50,7 @@ static void setAsHidden(string key)
 {
     using (var liteDb = new LiteDatabase(Path.GetFullPath("RufoList.db")))
     {
-        if (liteDb.CollectionExists("DBVariable"))
+        if (liteDb.CollectionExists("DBVariableStorage"))
         {
             var foundOne = liteDb.GetCollection<DBVariableStorage>().FindOne(x => x.Key == key);
             foundOne.SetAsHidden();
@@ -69,7 +69,7 @@ static string getLastValue(string key)
 {
     using (var liteDb = new LiteDatabase(Path.GetFullPath("RufoList.db")))
     {
-        if (liteDb.CollectionExists("DBVariable"))
+        if (liteDb.CollectionExists("DBVariableStorage"))
         {
             var foundOne = liteDb.GetCollection<DBVariableStorage>().FindOne(x => x.Key == key);
             string lastValue = foundOne.GetLastValue();
@@ -81,4 +81,39 @@ static string getLastValue(string key)
         }
     }
 }
+
+/*
+ - Assuming 100 different alarms
+    - numberOfTotalData: could be 6000 (day), 180 000 (month), 2200000(year), 6 600 000 (3years)
+    - randomValue: a random int between 1-10000
+ */
+
+Random rnd = new Random();
+int randomValue = 0;
+//int numOfTotalData = 6000; // for 1 day
+int numOfTotalData = 180000; // for 1 month
+//int numOfTotalData = 2200000; // for 1 year
+//int numOfTotalData = 6600000; // for 3 year
+int numOfTypesOfRegisters = 500;
+int numOfDataPerRegister = numOfTotalData / numOfTypesOfRegisters;
+
+Console.WriteLine($"Num Of Total data: {numOfTotalData}");
+Console.WriteLine($"Num Of Types of Registers: {numOfTypesOfRegisters}");
+Console.WriteLine($"Num Of Data Per Registers: {numOfDataPerRegister}");
+
+for (int i = 0; i < numOfTypesOfRegisters ; i++)
+{ 
+    string key = "Alarm"+i.ToString();
+
+    Console.WriteLine($" -  key: {key} , percentage: {(double) i*100/ numOfTypesOfRegisters}");
+
+    
+    for (int j = 0; j < numOfDataPerRegister; j++)
+    {
+
+        randomValue = rnd.Next(1, 10000);
+        AddData(key, randomValue.ToString());
+    }
+}
+
 
