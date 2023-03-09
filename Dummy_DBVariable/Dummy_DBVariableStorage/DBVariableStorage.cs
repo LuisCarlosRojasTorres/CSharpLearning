66e143c333330ae8f9efd17b0a3c9057b361e702
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2023-03-09 09:24:26 -0300

    LiteDBStorage How much storage is 3 years
    Changelog:
    - Added Project
    
    List of Changes:
    - Add DBVariableStorage class
        - Add random timestamp
    
    List of TODOs:
    - Add Test with 6 000 data (a day)
    - Add Test with 180 000 data (a month)
    - Add Test with 2 200 000 data (a year)
    - Add Test with 6 600 000 data (3 years)

---
+++ Dummy_DBVariable/Dummy_DBVariableStorage/DBVariableStorage.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dummy_DBVariableStorage
+{
+    internal class DBVariableStorage
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+
+        public List<string>? Dates { get; set; }
+        public List<string>? Values { get; set; }
+        public List<bool>? Hidden { get; set; }
+
+        public DBVariableStorage()
+        {
+            this.Key = "dummyKey";
+
+            Random rnd = new Random();
+            int year = rnd.Next(1, 9999);
+            int month = rnd.Next(1, 13);
+            int day = rnd.Next(1, 28);
+
+            int hour = rnd.Next(1, 24);
+            int minute = rnd.Next(1, 60);
+            int second = rnd.Next(1, 60);
+
+            this.Dates = new List<string> { (new DateTime(year, month, day, hour, minute, second)).ToString() };
+            this.Values = new List<string> { "dummyValue" };
+            this.Hidden = new List<bool> { false };
+        }
+
+        public DBVariableStorage(string key, List<string> dates, List<string> values, List<bool> hidden)
+        {
+            this.Key = key;
+
+            this.Dates = dates;
+            this.Values = values;
+            this.Hidden = hidden;
+        }
+
+        public DBVariableStorage(string key, string value, bool hidden = false)
+        {
+            this.Key = key;
+
+            Random rnd = new Random();
+            int year = rnd.Next(1, 9999);
+            int month = rnd.Next(1, 13);
+            int day = rnd.Next(1, 28);
+
+            int hour = rnd.Next(1, 24);
+            int minute = rnd.Next(1, 60);
+            int second = rnd.Next(1, 60);
+
+            this.Dates = new List<string> { (new DateTime(year, month, day, hour, minute, second)).ToString() };
+            this.Values = new List<string> { value };
+            this.Hidden = new List<bool> { hidden };
+        }
+
+        public void AddValue(string value, bool hidden = false)
+        {
+            Random rnd = new Random();
+            int year = rnd.Next(1, 9999);
+            int month = rnd.Next(1, 13);
+            int day = rnd.Next(1, 28);
+
+            int hour = rnd.Next(1, 24);
+            int minute = rnd.Next(1, 60);
+            int second = rnd.Next(1, 60);
+
+            this.Dates = new List<string> { (new DateTime(year, month, day, hour, minute, second)).ToString() };
+            this.Values.Add(value);
+            this.Hidden.Add(hidden);
+        }
+
+        public void SetAsHidden()
+        {
+            for (int i = 0; i < this.Hidden.Count; i++)
+            {
+                this.Hidden[i] = true;
+            }
+        }
+
+        public string GetLastValue()
+        {
+            if (this.Values != null)
+            {
+                return this.Values[this.Values.Count() - 1];
+            }
+            else
+            {
+                return "null reference!";
+            }
+
+        }
+
+        public override string ToString()
+        {
+            string ans = "KEY: " + Key + "\n";
+
+            for (int i = 0; i < Dates.Count; i++)
+            {
+                ans += " - " + Dates[i] + " , " + Values[i] + " , " + Hidden[i].ToString() + "\n";
+            }
+
+            return ans;
+        }
+    }
+}
--- Dummy_DBVariable/Dummy_DBVariableStorage/Program.cs
+++ Dummy_DBVariable/Dummy_DBVariableStorage/Program.cs
@@ -1,2 +1,84 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using LiteDB;
+using Dummy_DBVariableStorage
+
+static void AddData(string key, string value)
+{
+    //using (var liteDb = new LiteDatabase(Path.GetFullPath("Rufo.db"))) 
+    using (var liteDb = new LiteDatabase("RufoList.db"))
+    {
+        if (liteDb.CollectionExists("DBVariable"))
+        {
+            //Update
+            var foundOne = liteDb.GetCollection<DBVariableStorage>().FindOne(x => x.Key == key);
+
+            if (foundOne != null)
+            {
+                foundOne.AddValue(value);
+                liteDb.GetCollection<DBVariableStorage>().Update(foundOne);
+            }
+            else
+            {
+                liteDb.GetCollection<DBVariableStorage>().Insert(new DBVariableStorage(key, value));
+            }
+        }
+        else
+        {
+            //Add
+            liteDb.GetCollection<DBVariableStorage>().Insert(new DBVariableStorage(key, value));
+        }
+    }
+}
+
+static void printVariable(string key)
+{
+    using (var liteDb = new LiteDatabase(Path.GetFullPath("RufoList.db")))
+    {
+        if (liteDb.CollectionExists("DBVariable"))
+        {
+            var foundOne = liteDb.GetCollection<DBVariableStorage>().FindOne(x => x.Key == key);
+            Console.WriteLine(foundOne);
+        }
+        else
+        {
+            Console.WriteLine("Se llevaron todo, todillo");
+        }
+    }
+}
+
+static void setAsHidden(string key)
+{
+    using (var liteDb = new LiteDatabase(Path.GetFullPath("RufoList.db")))
+    {
+        if (liteDb.CollectionExists("DBVariable"))
+        {
+            var foundOne = liteDb.GetCollection<DBVariableStorage>().FindOne(x => x.Key == key);
+            foundOne.SetAsHidden();
+            liteDb.GetCollection<DBVariableStorage>().Update(foundOne);
+            Console.WriteLine(foundOne);
+
+        }
+        else
+        {
+            Console.WriteLine("Se llevaron todo, todillo");
+        }
+    }
+}
+
+static string getLastValue(string key)
+{
+    using (var liteDb = new LiteDatabase(Path.GetFullPath("RufoList.db")))
+    {
+        if (liteDb.CollectionExists("DBVariable"))
+        {
+            var foundOne = liteDb.GetCollection<DBVariableStorage>().FindOne(x => x.Key == key);
+            string lastValue = foundOne.GetLastValue();
+            return lastValue;
+        }
+        else
+        {
+            return "";
+        }
+    }
+}
+
