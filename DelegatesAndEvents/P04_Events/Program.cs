63182ffc78d95a43a28bc9cf62e5906af2ec4f7d
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-01-02 07:09:33 -0300

    Learning DelegatesAndEvents - Events with data
    Changelog
    - Solution Created
    - Added Delegates
    - Added AnonymousMethods
    - Added LambdaExpressions
    
    List of Changes
    - Add Events
    	- With data
    
    TODO
    - Add Events
    - Add MethodGroup conversion to delegate

--- DelegatesAndEvents/P04_Events/Program.cs
+++ DelegatesAndEvents/P04_Events/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace P04_Events
+﻿using System;
+
+namespace P04_Events
 {
     class DummyPublisher
     {
@@ -26,20 +28,67 @@
         }
     }
 
+    class DummyEventArgs : EventArgs
+    {
+        public string? stringVar { get; set; }
+        public int intVar { get; set; }
+    }
+
+    class DummyPublisherWithData
+    {
+        //The delegate declaration follows the signature from the previous section
+        public event EventHandler<DummyEventArgs>? dummyEvent;
+
+        //THe Dummy method now has parameters
+        public void DummyMethod(string dummyString, int dummyInt)
+        {
+            Console.WriteLine(" - DummyMethod with data");
+
+            // If there are methods referenced in the delegate:
+            // Otherwise nothing happens
+            if (dummyEvent != null)
+            {
+                dummyEvent(this, new DummyEventArgs { stringVar = dummyString, intVar = dummyInt });
+            }
+        }
+    }
+
+    class DummySubscriberWithData
+    {
+        //Same EventHandler signature
+        public static void SubscriberMethod(object sender, DummyEventArgs e)
+        {
+            Console.WriteLine($" - SubscriberMethod");
+            Console.WriteLine($"    - stringVar field: {e.stringVar}");
+            Console.WriteLine($"    - intVar field: {e.intVar}");
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            var dummyPublisher = new DummyPublisher();
+            var dummyPublisher = new DummyPublisherWithData();
 
             // Reference to EventHandler delegate
-            dummyPublisher.dummyEvent += DummySubscriber.SubscriberMethod;
+            dummyPublisher.dummyEvent += DummySubscriberWithData.SubscriberMethod!;
 
-            //Raise the event
-            dummyPublisher.DummyMethod();
+            //Raise the event withouth
+            //dummyPublisher.DummyMethod();
             //This will print:
             // - DummyMethod
             // -SubscriberMethod
+
+            //Raise the event with data
+            int dummyInt = 10;
+            string dummyString = "DummyString value";
+            dummyPublisher.DummyMethod(dummyString, dummyInt);
+            //This will print:
+            //-DummyMethod with data
+            //-SubscriberMethod
+            //- stringVar field: DummyString value
+            //-intVar field: 10
 
         }
     }
