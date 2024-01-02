d59bc6ad2cfc03540321b719eba095539ce731ba
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-01-02 08:27:31 -0300

    Learning DelegatesAndEvents - Events with data
    Changelog
    - Solution Created
    - Added Delegates
    - Added AnonymousMethods
    - Added LambdaExpressions
    - Add Events
    
    List of Changes
    - Add Many Events class
    
    TODO
    - Add Many Events class
    - Add MethodGroup conversion to delegate

---
+++ DelegatesAndEvents/P04b_ManyEvents/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace P04b_ManyEvents
+{
+    internal class Program
+    {
+        delegate void Dummy1EventHandler();
+        delegate void Dummy2EventHandler();
+
+        class DummyPublisher
+        {
+            // An Event has a delegate encapsulated
+            public event Dummy1EventHandler? dummy1EventHandler;
+            public event Dummy2EventHandler? dummy2EventHandler;
+
+            public void DummyMethod1()
+            {
+                Console.WriteLine(" - DummyPublisher: DummyMethod1");
+
+                if (dummy1EventHandler != null)
+                {
+                    dummy1EventHandler();
+                }
+            }
+
+            public void DummyMethod2()
+            {
+                Console.WriteLine(" - DummyPublisher: DummyMethod2");
+
+                if (dummy2EventHandler != null)
+                {
+                    dummy2EventHandler();
+                }
+            }
+        }
+
+        class DummySubscriber
+        {
+            public static void SubscriberMethod1()
+            {
+                Console.WriteLine(" - DummySubscriber: SubscriberMethod1");
+            }
+
+            public static void SubscriberMethod2()
+            {
+                Console.WriteLine(" - DummySubscriber: SubscriberMethod2");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var dummy = new DummyPublisher();
+
+            dummy.dummy1EventHandler += DummySubscriber.SubscriberMethod1;
+            dummy.dummy2EventHandler += DummySubscriber.SubscriberMethod2;
+            
+            dummy.DummyMethod1();
+            dummy.DummyMethod2();
+        }
+    }
+}
