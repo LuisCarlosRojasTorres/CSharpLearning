f26194f940ea788a378bc75116e87399930d9bdb
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-01-02 06:49:06 -0300

    Learning DelegatesAndEvents - Events without data
    Changelog
    - Solution Created
    - Added Delegates
    - Added AnonymousMethods
    - Added LambdaExpressions
    
    List of Changes
    - Add Events
    	- Without data
    
    TODO
    - Add Events
    - Add MethodGroup conversion to delegate

--- DelegatesAndEvents/P04_Events/Program.cs
+++ DelegatesAndEvents/P04_Events/Program.cs
@@ -1,10 +1,48 @@
 ﻿namespace P04_Events
 {
-    internal class Program
+    class DummyPublisher
+    {
+        public event EventHandler dummyEvent;
+
+        public void DummyMethod()
+        {
+            Console.WriteLine(" - DummyMethod");
+
+            // If there are methods referenced in the delegate:
+            // Otherwise nothing happens
+            if (dummyEvent != null)
+            {
+                dummyEvent(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    class DummySubscriber
+    {
+        //Same EventHandler signature
+        public static void SubscriberMethod(object sender, EventArgs e)
+        {
+            Console.WriteLine($" - SubscriberMethod");
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var dummyPublisher = new DummyPublisher();
+
+            // Reference to EventHandler delegate
+            dummyPublisher.dummyEvent += DummySubscriber.SubscriberMethod;
+
+            //Raise the event
+            dummyPublisher.DummyMethod();
+            //This will print:
+            // - DummyMethod
+            // -SubscriberMethod
+
         }
     }
+
+
 }
