701eea900eeff44f9877a66dc5eb0a8d6f624786
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-01-03 17:40:21 -0300

    Learning Concurrency - Threads
    Changelog
    
    List of Changes
    - Solution Created
    - Add MultiThreading
    	- Added Thread
    TODO
    - Add MultiThreading
    	- Add Thread with lambda expressions
    	- Add Background
    	- Add ThreadPool
    - Add Taskbased Asynchronous
    	- Add Task
        - Add Chain multiple tasks
        - Add Handle exceptions
        - Add AsyncAwait
        - Add Cancelling an Async operations
        - Add WhenAny
        - Add WhenAll
    - Add Synchronization
        - Add lock statement
        - Add deadlock
        - Add interlocked
        - Add readerWriterLockSlin
        - Add SemaphoreSlime
    - Add Thread Signaling
        - Add AutoResetEvent
        - Add ManualResetEvent
        - Add CountdownEvent

--- Concurrency/P06_Threads/Program.cs
+++ Concurrency/P06_Threads/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace P06_Threads
+﻿using System.Diagnostics;
+
+namespace P06_Threads
 {
-    internal class Program
+    class Program
     {
+        static void DummyMethod()
+        {
+            Console.WriteLine(" - DummyMethod...");
+            Thread.Sleep(2000);
+            Console.WriteLine(" - DummyMethod...done");
+        }
+        static void DummyMethodWithParameters(object o)
+        {
+            Console.WriteLine(" - DummyMethodWithParameters...");
+            Thread.Sleep(2000);
+            Console.WriteLine($" - DummyMethodWithParameters: o={o}");
+            Console.WriteLine(" - DummyMethodWithParameters...done");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ThreadStart startNoParam = DummyMethod;
+            ParameterizedThreadStart startParam= DummyMethodWithParameters!;
+            var watch = Stopwatch.StartNew();
+
+            var t1 = new Thread(startNoParam);
+            var t2 = new Thread(startParam);
+
+
+            // start both threads
+            t1.Start();
+            t2.Start(5);
+
+            // wait for both threads completed
+            t1.Join();
+            t2.Join();
+
+            watch.Stop();
+            Console.WriteLine($"It took {watch.Elapsed.Seconds} second(s) to complete.");
         }
     }
 }
