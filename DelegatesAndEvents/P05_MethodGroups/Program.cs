4856e3a56b89cca893fd5ad0ccdf603576e2a474
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-01-03 14:40:14 -0300

    Learning DelegatesAndEvents - MethodGroups
    Changelog
    - Solution Created
    - Added Delegates
    - Added AnonymousMethods
    - Added LambdaExpressions
    - Added Events
    
    List of Changes
    - Add MethodGroup conversion to delegate
    
    TODO

---
+++ DelegatesAndEvents/P05_MethodGroups/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P05_MethodGroups
+{
+    class Calculator
+    {
+    public static int Add(int a, int b) => a + b;
+    public static double Add(double a, double b) => a + b;
+    }
+
+    class Program
+    {
+        delegate int DummyDelegate(int a, int b);
+
+        public static void Main(string[] args)
+        {
+        //This signature match with first Add method
+        
+        //Method group assigned to delegate
+        DummyDelegate dummy = Calculator.Add;
+
+        //The Add method that best batch the delegate signature is referenced!        
+        var result = dummy(10, 20);
+
+        Console.WriteLine(result);
+        }
+    }
+}
