19c5f26b298dc26162d4bcd93915b83127a0ba11
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-01-04 11:36:46 -0300

    Learning Concurrency - Task
    Changelog
    - Solution Created
    - Add MultiThreading
    	- Added Thread
    	- Added Thread with lambda expressions
    	- Added BackgroundThread
    	- Added ThreadPool
    List of Changes
    - Add Taskbased Asynchronous
    	- Added Task
    TODO
    - Add Taskbased Asynchronous
    	- Add Chain multiple tasks
        - Add Handle exceptions
        - Add AsyncAwait
        - Add Cancelling an Async operations
        - Add WhenAny
        - Add WhenAll
    - Add Synchronization
        - Add lock statement
        - Add deadlock
        - Add interlocked
        - Add readerWriterLockSlin
        - Add SemaphoreSlime
    - Add Thread Signaling
        - Add AutoResetEvent
        - Add ManualResetEvent
        - Add CountdownEvent

---
+++ Concurrency/P09_Task/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace P09_Task
+{
+    public class Program
+    {
+        static int GetRandomNumber()
+        {
+            Thread.Sleep(1000);
+            int randomNumber = (new Random()).Next(1, 100);
+            Console.WriteLine($" - GetRandomNumber: The random number is {randomNumber}");
+            return randomNumber;
+        }
+
+        static int DummyMethod(int n)
+        {
+            Console.WriteLine(" - DummyMethodWithParameters:...");
+            Thread.Sleep(2000);
+            Console.WriteLine($" - DummyMethodWithParameters: n={n}");
+            Console.WriteLine(" - DummyMethodWithParameters:...done");
+            return n;
+        }
+
+        static void Main(string[] args)
+        {
+            int dummyInputValue = 15;
+            //var task1 = new Task(() => DummyMethod(dummyInputValue));
+            //task1.Start();
+
+            var task2 = Task.Run(() => DummyMethod(dummyInputValue));
+            
+
+            Console.WriteLine("Start the main program...");
+            Console.WriteLine($" - Task.Run: Result = {task2.Result}");
+            //Console.ReadLine();
+        }
+    }
+}
