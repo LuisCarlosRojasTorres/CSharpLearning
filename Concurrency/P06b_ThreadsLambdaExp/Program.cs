d652e3ff47a55e9872b2b12ab15f63efa027ac3a
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-01-04 07:10:01 -0300

    Learning Concurrency - BackgroundThread
    Changelog
    - Solution Created
    - Add MultiThreading
    	- Added Thread
    	- Added Thread with lambda expressions
    
    List of Changes
    - Add MultiThreading
    	- Added BackgroundThread
    
    TODO
    - Add MultiThreading
    	- Add ThreadPool
    - Add Taskbased Asynchronous
    	- Add Task
        - Add Chain multiple tasks
        - Add Handle exceptions
        - Add AsyncAwait
        - Add Cancelling an Async operations
        - Add WhenAny
        - Add WhenAll
    - Add Synchronization
        - Add lock statement
        - Add deadlock
        - Add interlocked
        - Add readerWriterLockSlin
        - Add SemaphoreSlime
    - Add Thread Signaling
        - Add AutoResetEvent
        - Add ManualResetEvent
        - Add CountdownEvent

---
+++ Concurrency/P06b_ThreadsLambdaExp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+namespace P06b_ThreadsLambdaExp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadStart startNoParam = () => {
+                Console.WriteLine(" - DummyMethod...");
+                Thread.Sleep(2000);
+                Console.WriteLine(" - DummyMethod...done");
+            };
+            
+            ParameterizedThreadStart startParam = (object? o) => {
+                Console.WriteLine(" - DummyMethodWithParameters...");
+                Thread.Sleep(2000);
+                Console.WriteLine($" - DummyMethodWithParameters: o={o}");
+                Console.WriteLine(" - DummyMethodWithParameters...done");
+            };
+
+            var watch = Stopwatch.StartNew();
+
+            var t1 = new Thread(startNoParam);
+            var t2 = new Thread(startParam);
+
+
+            // start both threads
+            t1.Start();
+            t2.Start(5);
+
+            // wait for both threads completed
+            t1.Join();
+            t2.Join();
+
+            watch.Stop();
+            Console.WriteLine($"It took {watch.Elapsed.Seconds} second(s) to complete.");
+        }
+    }
+}
---
+++ Concurrency/P07_BackgroundThreads/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace P07_BackgroundThreads
+{
+    public class Program
+    {
+        static void DummyMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine(" - DummyMethod in Background thread running...");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var dummyThread = new Thread(DummyMethod);
+            dummyThread.IsBackground = true;
+            dummyThread.Start();
+
+            // Do some work in the main thread
+            for (int i = 0; i < 2; i++)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine("Main thread running...");
+            }
+        }
+    }
+}
