b4b9b8fe1af52807fec96dabf8557b5cdaa4cb91
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-01-04 10:10:25 -0300

    Learning Concurrency - ThreadPool
    Changelog
    - Solution Created
    - Add MultiThreading
    	- Added Thread
    	- Added Thread with lambda expressions
    	- Added BackgroundThread
    List of Changes
    - Added MultiThreading
    	- Added ThreadPool
    
    TODO
    - Add Taskbased Asynchronous
    	- Add Task
        - Add Chain multiple tasks
        - Add Handle exceptions
        - Add AsyncAwait
        - Add Cancelling an Async operations
        - Add WhenAny
        - Add WhenAll
    - Add Synchronization
        - Add lock statement
        - Add deadlock
        - Add interlocked
        - Add readerWriterLockSlin
        - Add SemaphoreSlime
    - Add Thread Signaling
        - Add AutoResetEvent
        - Add ManualResetEvent
        - Add CountdownEvent

---
+++ Concurrency/P08_ThreadPool/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace P08_ThreadPool
+{
+    public class Program
+    {
+        static void DummyMethod(int n)
+        {
+            Console.WriteLine(" - DummyMethodWithParameters...");
+            Thread.Sleep(2000);
+            Console.WriteLine($" - DummyMethodWithParameters: n={n}");
+            Console.WriteLine(" - DummyMethodWithParameters...done");
+        }
+
+        static void Main(string[] args)
+        {
+            ParameterizedThreadStart parameterizedThreadStart = (object? o) => {
+                Console.WriteLine(" - DummyMethodWithParameters...");
+                Thread.Sleep(2000);
+                Console.WriteLine($" - DummyMethodWithParameters: o={o}");
+                Console.WriteLine(" - DummyMethodWithParameters...done");
+            };
+
+            int value = 10;
+            ThreadPool.QueueUserWorkItem(input =>
+                {
+                    parameterizedThreadStart(value);
+                    //DummyMethod(value);
+                }
+            );
+
+            Console.ReadLine();
+        }
+    }
+}
