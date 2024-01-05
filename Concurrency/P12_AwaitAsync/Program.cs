afecf83bc7651ade129c97abcd416e68c7aeaf35
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-01-05 17:03:57 -0300

    Learning Concurrency - AsyncAwait
    Changelog
    - Solution Created
    - Add MultiThreading
    	- Added Thread
    	- Added Thread with lambda expressions
    	- Added BackgroundThread
    	- Added ThreadPool
    - Add Taskbased Asynchronous
    	- Added Task
    	- Added Chain multiple tasks
    	- Added Handle exceptions
    
    List of Changes
    - Add Taskbased Asynchronous
    	- Added AsyncAwait
    
    TODO
    - Add Taskbased Asynchronous
        - Add Cancelling an Async operations
        - Add WhenAny
        - Add WhenAll
    - Add Synchronization
        - Add lock statement
        - Add deadlock
        - Add interlocked
        - Add readerWriterLockSlin
        - Add SemaphoreSlime
    - Add Thread Signaling
        - Add AutoResetEvent
        - Add ManualResetEvent
        - Add CountdownEvent

---
+++ Concurrency/P12_AwaitAsync/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P12_AwaitAsync
+{
+    internal class Program
+    {
+        
+        static async Task DummyMethod(string filename)
+        {
+            Console.WriteLine(" - DummyMethod: ");            
+            var lines = await File.ReadAllLinesAsync(filename);
+
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine(" - DummyMethod:... done!");
+        }
+
+        static async Task Main()
+        {
+            Console.WriteLine(" - Main Thread: ");
+            await DummyMethod("readme.txt");
+            Console.WriteLine(" - Main Thread:...done!");            
+        }
+    }
+}
---
+++ Concurrency/P12_AwaitAsync/readme.txt.txt
@@ -0,0 +1,7 @@
+{
+  "Ip": "127.0.0.1",
+  "Port": 1883,
+  "Topic": "UpdaterPackage",
+  "FileToTransfer": "./dummyDirectory/dummyFile2.png",
+  "DirectoryToTransfer": "./dummyDirectory/"
+}
