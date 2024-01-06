127ce3e321413aeef2e8869086042a401acb7ccc
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-01-06 11:24:57 -0300

    Learning Concurrency - AutoResetEventWithThreads
    Changelog
    - Solution Created
    - Add MultiThreading
    	- Added Thread
    	- Added Thread with lambda expressions
    	- Added BackgroundThread
    	- Added ThreadPool
    - Add Taskbased Asynchronous
    	- Added Task
    	- Added Chain multiple tasks
    	- Added Handle exceptions
    	- Added AsyncAwait
    	- Added Cancelling an Async operations
    
    List of Changes
    - Add Thread Signaling
        - Add AutoResetEventWithThreads
    
    TODO
    - Add Taskbased Asynchronous
        - Add WhenAny
        - Add WhenAll
    - Add Synchronization
        - Add lock statement
        - Add deadlock
        - Add interlocked
        - Add readerWriterLockSlin
        - Add SemaphoreSlime
    - Add Thread Signaling
        - Add AutoResetEventWithTasks
        - Add ManualResetEvent
        - Add CountdownEvent

---
+++ Concurrency/P14_AutoResetEvent/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace P14_AutoResetEvent
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+
+            void DummyMethod1()
+            {
+                int counter = 1;
+                while (true)
+                {
+                    Thread.Sleep(1000);
+                    if (counter % 5 != 0)
+                    {
+                        Console.WriteLine($" - Thread1: Second {counter}");
+                        counter++;
+                    }
+                    else
+                    {
+                        Console.WriteLine($" - Thread1: WaitOne {counter}");
+                        autoResetEvent.WaitOne();
+                        counter = 1;
+                    }
+                }
+            }
+
+            void DummyMethod2()
+            {
+                int counter = 1;
+                while (true)
+                {
+                    Thread.Sleep(1000);
+                    if (counter % 10 == 0)
+                    {
+                        Console.WriteLine($" - Thread2: Set AutoResetEvent for Thread1 every 10 seconds");
+                        autoResetEvent.Set();
+                        counter = 1;
+                    }
+                    else
+                    {                        
+                        counter ++ ;
+                    }
+
+                }
+            }
+
+            /*
+            // Thread Version  
+            var t1 = new Thread(DummyMethod1);
+            var t2 = new Thread(DummyMethod2);
+
+            t1.Start();
+            t2.Start();
+            */
+
+            // Task Version
+            var task1 = Task.Run(DummyMethod1);
+            var task2 = Task.Run(DummyMethod2);
+
+            Console.Read();
+        }
+    }
+}
