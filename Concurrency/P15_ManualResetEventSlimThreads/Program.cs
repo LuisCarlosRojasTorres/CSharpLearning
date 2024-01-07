e9344e955fd59d117d2cc021a2977e903c993609
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-01-07 05:43:10 -0300

    Learning Concurrency - ManualResetEventWithThreads
    Changelog
    - Solution Created
    - Added MultiThreading
    	- Added Thread
    	- Added Thread with lambda expressions
    	- Added BackgroundThread
    	- Added ThreadPool
    - Added Taskbased Asynchronous
    	- Added Task
    	- Added Chain multiple tasks
    	- Added Handle exceptions
    	- Added AsyncAwait
    	- Added Cancelling an Async operations
    - Added Thread Signaling
        - Add AutoResetEventWithThreads
    	- Added AutoResetEventWithTasks
    
    List of Changes
    - Added Thread Signaling
        - Added ManualResetEventWithThreads
    
    TODO
    - Add Taskbased Asynchronous
        - Add WhenAny
        - Add WhenAll
    - Add Synchronization
        - Add lock statement
        - Add deadlock
        - Add interlocked
        - Add readerWriterLockSlin
        - Add SemaphoreSlime
    - Add Thread Signaling
        - Add ManualResetEventWithTasks
        - Add CountdownEvent

---
+++ Concurrency/P15_ManualResetEventSlimThreads/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace P15_ManualResetEventSlimThreads
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            ManualResetEventSlim manualResetEventSlim = new ManualResetEventSlim(false);
+
+            void DummyMethod1()
+            {
+                int counter = 1;
+                while (true)
+                {
+                    Thread.Sleep(1000);
+                    if (counter % 5 != 0)
+                    {
+                        Console.WriteLine($" - Thread1: Second {counter}");
+                        counter++;
+                    }
+                    else
+                    {
+                        Console.WriteLine($" - Thread1: WaitOne {counter}");
+                        manualResetEventSlim.Wait();
+                        counter = 1;
+                    }
+                }
+            }
+
+            void DummyMethod2()
+            {
+                int counter = 1;
+                while (true)
+                {
+                    Thread.Sleep(1000);
+                    if (counter % 11 == 0)
+                    {
+                        if (counter % 22 == 0)
+                        {
+                            Console.WriteLine($" - Thread2: Reset ManualResetEventSlim for Thread1 every 20 seconds");
+                            manualResetEventSlim.Reset();
+                            counter = 1;
+                        }
+                        else 
+                        {
+                            Console.WriteLine($" - Thread2: Set ManualResetEventSlim for Thread1 every 10 seconds");
+                            manualResetEventSlim.Set();
+                            counter++;
+                        }
+                    }
+                    else
+                    {
+                        counter++;
+                    }                    
+                }
+            }
+
+            // Thread Version  
+            var t1 = new Thread(DummyMethod1);
+            var t2 = new Thread(DummyMethod2);
+
+            t1.Start();
+            t2.Start();
+
+            // Task Version
+            //var task1 = Task.Run(DummyMethod1);
+            //var task2 = Task.Run(DummyMethod2);
+            //Console.Read();
+        }
+    }
+}
